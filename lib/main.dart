4d72a5d07fff280eeea105725f4cda2ab27dcc0d
Itthipon Treetodbadee <>
2020-06-12 23:10:14 +0700

    Quize app stateless stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,94 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+// dart programming language
+
+void main() => runApp(MyClass());
+
+class MyClass extends StatefulWidget {
+  @override
+  _MyClassState createState() => _MyClassState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyClassState extends State<MyClass> {
+  int questionIndex = 0;
+
+  var questions = [
+    {
+      'questText': 'What\'s your favourite color?',
+      'anwsers': ['Red', 'Green', 'Blue', 'Black']
+    },
+    {
+      'questText': 'What\'s your favourite animal?',
+      'anwsers': ['Cat', 'Dog', 'Rabbit']
+    },
+  ];
+
+  answerQuestion() {
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'MyApp',
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('My App'),
+          ),
+          body: Container(
+            child: Column(
+              children: [
+                Text(
+                  questions[questionIndex]['questText'],
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                ...(questions[questionIndex]['anwsers'] as List<String>)
+                    .map((text) {
+                  return Container(
+                    child: RaisedButton(
+                      onPressed: answerQuestion,
+                      child: Text(text),
+                      color: Colors.blue,
+                      textColor: Colors.white,
+                    ),
+                    width: double.infinity,
+                    margin: EdgeInsets.only(left: 10, right: 10),
+                  );
+                }),
+              ],
+            ),
+            width: double.infinity,
+            alignment: Alignment.topCenter,
+          )),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+/*
+// print('sadifjioawejf');
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  // var obj = MyClass(firstname: 'sdfsdfjoi', num1: 12);
+  // obj.myFunction3(d: 2, a: 1, b: 4);
+class MyClass {
+  String firstname;
+  int num1;
+  List<String> x = ['a', 'b', 'c'];
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyClass({this.firstname, this.num1}) {}
 
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  int myFunction() {
+    x.map((e)  {print(e);});
+    return 2;
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
+  int myFunction1() => 2;
+
+  myFunction3({int a, int b, int c, int d}) {}
 }
+*/
